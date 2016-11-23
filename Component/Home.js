34dbf1abce62da2bac86635d9e665041f7c4c518
noprom <tyee.noprom@qq.com>
2016-11-23 10:51:19 +0800

    add news list

--- Component/Home.js
+++ Component/Home.js
@@ -22,28 +22,62 @@ var Home = React.createClass({
     },
 
     // 初始化
-    getInitialState() {
-        return {
+    getInitialState(){
+        return{
             // ListView头部轮播图的数据源
-            headerDataArr: [],
+            headerDataArr:[],
             // cell的数据源
             dataSource: new ListView.DataSource({
-                rowHasChanged:(r1, r2) => {r1 !== r2}
+                rowHasChanged:(r1,r2)=>{r1 !== r2}
             })
         }
     },
 
-    // 组件加载完毕之后
-    componentDidMount() {
+    render() {
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderRow}
+                renderHeader={this.renderHeader}
+            />
+        );
+    },
+
+    // 返回ListView头部视图
+    renderHeader(){
+        return(
+            <View>
+                <Text>头部视图</Text>
+            </View>
+        )
+    },
+
+    // 返回LisView中的单个cell
+    renderRow(rowData){
+        return(
+            <TouchableOpacity activeOpacity={0.5}>
+                <View style={styles.cellViewStyle}>
+                    <Image source={{uri:rowData.imgsrc}} style={styles.imgStyle} />
+                    <View style={styles.rightViewStyle}>
+                        <Text style={styles.mainTitleStyle}>{rowData.title}</Text>
+                        <Text style={styles.subTitleStyle}>{rowData.digest}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+        )
+    },
+
+    // 组件加载完毕之后调用
+    componentDidMount(){
         // 请求网络数据
         this.loadDataFromNet();
     },
 
     // 请求网络数据的方法
-    loadDataFromNet() {
+    loadDataFromNet(){
         fetch(this.props.url_api)
-            .then((response) => response.json())
-            .then((responseData) => {
+            .then((response)=>response.json())
+            .then((responseData)=>{
                 // 拿到需要的数据
                 var jsonData = responseData[this.props.key_word];
 
@@ -51,7 +85,8 @@ var Home = React.createClass({
                 this.dealWithData(jsonData);
             })
             .catch((error)=>{
-                if(error){
+                console.log(error);
+                if(error) {
                    // 网络请求失败,就用本地数据
                     console.log('网络请求失败');
                     var jsonData = LocalData[this.props.key_word];
@@ -61,11 +96,11 @@ var Home = React.createClass({
     },
 
     // 处理网络数据的细节方法
-    dealWithData(jsonData) {
+    dealWithData(jsonData){
         // 定义临时变量
         var headerArr = [], listDataArr = [];
         // 遍历拿到的json数据
-        for (var i = 0; i < jsonData.length; i++) {
+        for (var i=0;i<jsonData.length;i++){
             // 取出单个对象
             var data = jsonData[i];
             if(data.hasAD == 1){
@@ -80,36 +115,41 @@ var Home = React.createClass({
         // 更新状态机
         this.setState({
             // ListView头部轮播图的数据源
-            headerDataArr: headerArr,
+            headerDataArr:headerArr,
             // cell的数据源
-            dataSource: this.state.dataSource.cloneWithRows(listDataArr),
+            dataSource:this.state.dataSource.cloneWithRows(listDataArr),
         });
 
-        console.log(headerArr,listDataArr);
+        // console.log(headerArr,listDataArr);
     },
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    首页
-                </Text>
-            </View>
-        );
-    }
 });
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+    cellViewStyle:{
+        // 主轴方向
+        flexDirection:'row',
+        padding:10,
+        // 侧轴对齐方式
+        alignItems:'center',
+        // 设置下边框
+        borderBottomColor:'#e8e8e8',
+        borderBottomWidth:0.8,
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    imgStyle:{
+        width:90,
+        width:90,
+    },
+    rightViewStyle:{
+        width:260,
+        marginLeft:10,
+    },
+    mainTitleStyle:{
+        fontSize:16,
+        marginBottom:5,
+    },
+    subTitleStyle:{
+        fontSize:14,
+        color:'gray',
     },
 });
 
